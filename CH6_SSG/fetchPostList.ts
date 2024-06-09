7f068f17103ed061aa06956d95ad2e3316e5550f
TK-moon <tkmoon.dev@gmail.com>
2024-06-09 20:58:11 +0900

    Part9 강의 자료 추가

--- CH6_SSG/fetchPostList.ts
+++
@@ -1,8 +0,0 @@
-import { Post } from "./types";
-
-export async function fetchPostList(): Promise<Post[]> {
-  const result = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data: Post[] = await result.json();
-
-  return data;
-}
